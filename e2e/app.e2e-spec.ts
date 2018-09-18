f8a89ffa796df2710aea2627e6f40c5b0ee65e21
Ubuntu <ubuntu@ip-172-31-93-24.ec2.internal>
2018-09-18 14:32:40 +0000

    added e2e test cases for login and dashboard

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,40 @@
 import { AppPage } from "./app.po";
+import { browser } from 'protractor';
 
 describe("question-n-answers App", () => {
   let page: AppPage;
+  let password = 'Test@1234';
+  let email = 'dhiman@t.com';
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it("should display welcome message", () => {
+  it("should display app heading", () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual("Welcome to app!");
+    expect(page.getHeadingText()).toEqual("QnA");
   });
+
+
+  it("should navigate to the login page", () => {
+    expect(browser.getCurrentUrl()).toContain('/#/login');
+  });
+
+  it("should be able to login", () => {
+    page.getEmailField().sendKeys(email);
+    page.getPasswordField().sendKeys(password);
+    page.clickLoginButton();
+    expect(browser.getCurrentUrl()).toContain('/#/dashboard');
+  });
+
+  it("should be able to view 5 topics", () => {
+    expect(page.getTopicCards().count()).toEqual(5);
+  });
+
+  it("should be able to view details", () => {
+    page.clickViewTopicDetailsButton();
+    expect(browser.getCurrentUrl()).toContain('/#/topic-details');
+  });
+
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,56 @@ export class AppPage {
     return browser.get("/");
   }
 
-  getParagraphText() {
-    return element(by.css("app-root h1")).getText();
+  getHeadingText() {
+    return element(by.id(".app-heading")).getText();
   }
+
+  getFirstNameField(){
+    return element(by.id('#first-name-field'));
+  }
+
+  getLastNameField(){
+    return element(by.id('#last-name-field'));
+  }
+
+  getEmailField(){
+    return element(by.id('#email-field'));
+  }
+
+  getPasswordField(){
+    return element(by.id('#password-field'));
+  }
+
+  getRetypePasswordField(){
+    return element(by.id('#retype-password-field'));
+  }
+
+  clickLoginButton(){
+    return element(by.id('#btn-login')).click();
+  }
+
+  clickSignUpButton(){
+    return element(by.id('#btn-signup')).click();
+  }
+
+  getTopicCards(){
+    return element.all(by.id('.topic-card'));
+  }
+
+  getQuestionCards(){
+    return element.all(by.id('.question-card'));
+  }
+
+  getCommentCards(){
+    return element.all(by.id('.comment-card'));
+  }
+
+  clickViewTopicDetailsButton(){
+    element.all(by.id('.view-topic-details')).first().click();
+  }
+
+  clickViewCommentsButton(){
+    element.all(by.id('.view-comments')).first().click();
+  }
+
 }
