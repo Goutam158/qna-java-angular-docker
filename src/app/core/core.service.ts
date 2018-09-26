f7a098084f0962aecf9839b7c873aa8f8c7f484c
Ubuntu <ubuntu@ip-172-31-93-24.ec2.internal>
2018-09-26 08:35:01 +0000

    removed console.log

--- src/app/core/core.service.ts
+++ src/app/core/core.service.ts
@@ -50,19 +50,16 @@ export class CoreService {
   }
 
   addTopic(topic:TopicModel):Observable<any>{
-    console.log(topic);
     return this._http
     .post(`${this.authEndpoint}/topic`,topic,{responseType : 'text'});
   }
 
   addQuestion(question:QuestionModel):Observable<any>{
-    console.log(question);
     return this._http
     .post(`${this.authEndpoint}/question`,question,{responseType : 'text'});
   }
 
   addComment(comment:CommentModel):Observable<any>{
-    console.log(comment);
     return this._http
     .post(`${this.authEndpoint}/comment`,comment,{responseType : 'text'});
   }
